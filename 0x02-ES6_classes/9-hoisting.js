6b98a00dddb5255f872efbcaae240ef09f0abe2f
HebaBorhan <h_borhan@hotmail.com>
2024-07-28 01:41:48 +0300

    fix code

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,5 +1,5 @@
 export class HolbertonClass {
-  constructor(year, location) {
+  letructor(year, location) {
     this._year = year;
     this._location = location;
   }
@@ -14,7 +14,7 @@ export class HolbertonClass {
 }
 
 export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
+  letructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -33,15 +33,15 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
+let class2019 = new HolbertonClass(2019, 'San Francisco');
+let class2020 = new HolbertonClass(2020, 'San Francisco');
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+let student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+let student2 = new StudentHolberton('John', 'Doe', class2020);
+let student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+let student4 = new StudentHolberton('Donald', 'Bush', class2019);
+let student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
+let listOfStudents = [student1, student2, student3, student4, student5];
 
 export default listOfStudents;
