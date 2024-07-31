c95fa0486be9f95122193b81aee54add58d09fb0
HebaBorhan <h_borhan@hotmail.com>
2024-07-31 19:15:14 +0300

    Task 2 updated - main ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,8 +12,20 @@ const teacher3: Teacher = {
     lastName: 'Doe',
     location: 'London',
     contract: false,
-  };
+};
 
-  console.log(teacher3);
-  
+console.log(teacher3);
 
+interface Directors extends Teacher {
+    numberOfReports: number
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
